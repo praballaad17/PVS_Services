08742c6fb8664b57473cc5f648f4ce5db48dba16
prabal laad <praballaad17@gmail.com>
2021-02-05 23:16:49 +0530

    all pages completed

--- creative/server.js
+++ creative/server.js
@@ -6,9 +6,8 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
-//const url = 'mongodb+srv://user:5TvLUImh8JtcYG5G@cluster0.b5wat.mongodb.net/getesolution?retryWrites=true&w=majority'
+const url = 'mongodb+srv://user:3u4Rb3ZK9t5k3lil@cluster0.w5t2i.mongodb.net/PVS?retryWrites=true&w=majority'
 
-// const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -18,26 +17,32 @@ app.use(express.static(__dirname + '/assets'));
 // app.use(expressLayouts)
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}) )
 
-// const mongoose = require('mongoose')
-// mongoose.connect( url , { useNewUrlParser: true });
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to mongoose'))
+const mongoose = require('mongoose')
+mongoose.connect( url , { useNewUrlParser: true });
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
 
 app.post('/sign_up', function(req,res){ 
-    var name = req.body.name; 
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var companyName = req.body.companyName;
+    var jobTitle = req.body.jobTitle; 
+    var number =req.body.number;
     var email =req.body.email; 
-    var subject = req.body.subject; 
     var message =req.body.message; 
     var date = new Date();
     var data = { 
-        "name": name, 
+        "firstName": firstName,
+        "lastName":lastName,
+        "companyName":companyName,
+        "jobTitle":jobTitle,
         "email":email, 
-        "subject":subject, 
+        "number":number, 
         "message":message,
         "date": date 
     } 
-db.collection('details').insertOne(data,function(err, collection){ 
+db.collection('customers').insertOne(data,function(err, collection){ 
         if (err) throw err; 
         console.log("Record inserted Successfully"); 
               
@@ -49,15 +54,14 @@ db.collection('details').insertOne(data,function(err, collection){
 app.get('/', function(req, res) {
     res.render('index', { layout: 'specific-layout' });
 });
-// app.get('/services', function(req, res) {
-//     res.render('services/services');
-// });
+app.get('/services', function(req, res) {
+    res.render('services');
+});
 app.get('/contact', function(req, res) {
     res.render('contact');
 });
-// app.get('/', function(req, res) {
-//     res.render('index');
-//});
+app.get('/about', function(req, res) {
+    res.render('about');
+});
 
-// app.use('/', indexRouter) 
 app.listen(process.env.PORT || 3001)
